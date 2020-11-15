c5375c1c7f9b7704dd70abce974bab53ac2fb0d7
dheer meena <dheer1998meena@gmail.com>
2020-11-15 20:31:38 +0530

    [Dheer] Add: Implemented the functionality to add multiple employee to the table without thread

--- EmployeePayrollMultiThreadingMsTest/UnitTest1.cs
+++ EmployeePayrollMultiThreadingMsTest/UnitTest1.cs
@@ -1,3 +1,9 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Dheer Singh Meena"/>
+// --------------------------------------------------------------------------------------------------------------------
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EmployeePayrollProblem_MultiThreading;
 using System.Collections.Generic;
@@ -22,8 +28,8 @@ namespace EmployeePayrollMultiThreadingMsTest
             employeeList.Add(new EmployeeModel(employeeName: "Rohit", startDate: new System.DateTime(2019, 08, 01), phoneNumber: 4567876543, address: "Bangalore", department: "HR", gender: "M", basicPay: 50000, deductions: 2000, taxablePay: 48000, tax: 1000, netPay: 47000));
             employeeList.Add(new EmployeeModel(employeeName: "Sachin", startDate: new System.DateTime(2020, 01, 01), phoneNumber: 4563784678, address: "Chennai", department: "HR", gender: "M", basicPay: 50000, deductions: 2000, taxablePay: 48000, tax: 1000, netPay: 47000));
             employeeList.Add(new EmployeeModel(employeeName: "Yuvraj", startDate: new System.DateTime(2020, 02, 01), phoneNumber: 7865678765, address: "Mumbai", department: "Sales", gender: "M", basicPay: 60000, deductions: 2000, taxablePay: 58000, tax: 1000, netPay: 57000));
-            employeeList.Add(new EmployeeModel(employeeName: "Bumrah", startDate: new System.DateTime(2019, 02, 01), phoneNumber: 3456765434, address: "Delhi", department: "Marketing", gender: "F", basicPay: 60000, deductions: 2000, taxablePay: 58000, tax: 1000, netPay: 57000));
-            employeeList.Add(new EmployeeModel(employeeName: "Pery", startDate: new System.DateTime(2020, 04, 12), phoneNumber: 4567898754, address: "Bangalore", department: "Sales", gender: "M", basicPay: 60000, deductions: 2000, taxablePay: 58000, tax: 1000, netPay: 57000));
+            employeeList.Add(new EmployeeModel(employeeName: "Bumrah", startDate: new System.DateTime(2019, 02, 01), phoneNumber: 3456765434, address: "Delhi", department: "Marketing", gender: "M", basicPay: 60000, deductions: 2000, taxablePay: 58000, tax: 1000, netPay: 57000));
+            employeeList.Add(new EmployeeModel(employeeName: "Pery", startDate: new System.DateTime(2020, 04, 12), phoneNumber: 4567898754, address: "Bangalore", department: "Sales", gender: "F", basicPay: 60000, deductions: 2000, taxablePay: 58000, tax: 1000, netPay: 57000));
             bool expected = true;
             Stopwatch stopwatch = new Stopwatch();
 
